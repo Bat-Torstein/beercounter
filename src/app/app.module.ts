e03950ea9f134e35a97ab8b6b76805d1b28f294f
Torstein Bjørgan <torsteib@gmail.com>
2018-10-28 10:09:53 +0100

    removes unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {
-  SocialLoginModule,
-  AuthServiceConfig,
-  GoogleLoginProvider,
-  FacebookLoginProvider,
-} from 'angular-6-social-login';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    SocialLoginModule
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
